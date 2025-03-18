196bf3b147c4bf11ba679bbde3fc7cde90ecbea2
Jordy Ruiz <jr03@uw.edu>
2025-03-18 14:36:56 -0700

    Testing DaisyUI & TailwindCSS

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import cookieParser from "cookie-parser";
 import{connectDB} from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT; 
 
 // Middleware
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Server is running on PORT: ' + PORT);
     connectDB();
 });
