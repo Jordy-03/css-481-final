b44a10ebee122efede17f3457938becbc959ef36
Shisu123 <yapjojus@uw.edu>
2025-03-19 20:29:13 -0700

    fixed bug where chats linger when switching

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -2,7 +2,6 @@ import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-
 import { useAuthStore } from "../store/useAuthStore";
 import { formatTime } from "../lib/utils";
 
@@ -34,48 +33,51 @@ const ChatWindow = () => {
     }, [messages]);
 
     return (
-        <div className="flex-1 flex flex-col overflow-auto">
+        <div className="flex-1 flex flex-col overflow-hidden">
             <ChatHeader />      {/* Display ChatHeader */}
 
             <div className="flex-1 overflow-y-auto p-4 space-y-4">
-                {messages.map((message) => (
-                    <div
-                        key={message._id}
-                        className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-                        ref={messageEndRef} // auto scroll to last message
-                    > 
-                        <div className="chat-image avatar">
-                            {/* Display sender's avatar */}
-                            <div className="size-10 rounded-xl border">
-                                <img 
-                                    src={message.senderId === authUser._id ? 
-                                            authUser.profilePicture || "/vite.svg" 
-                                            : 
-                                            selectedUser.profilePicture || "/vite.svg"} 
-                                    alt="Profile picture" />
-                            </div>
-                        </div>
-                        <div className="chat-header mb-1">
-                            <time className="text-xs opacity-50 ml-l">
-                                {formatTime(message.createdAt)}
-                            </time>
-                        </div>
-                        {/* Chat Bubble */}
-                        <div className="chat-bubble flex flex-col">
-                            {/* Display message image */}
-                            {message.image && (
-                                <img 
-                                    src={message.image} 
-                                    alt="chat" 
-                                    className="sm:max-w-[200px] rounded-md mb-2" 
-                                />
-                            )}
-                            {/* Display message text */}
-                            {message.text && <p>{message.text}</p>}
-                        </div>
+                {messagesLoading ? (
+                    <div className="flex justify-center items-center h-full">
+                        <span className="loading loading-spinner loading-lg"></span>
                     </div>
-
-                ))}
+                ) : (
+                    <>
+                        {messages.map((message, index) => (
+                            <div
+                                key={message._id}
+                                className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+                                ref={index === messages.length - 1 ? messageEndRef : null}
+                            > 
+                                <div className="chat-image avatar">
+                                    <div className="size-10 rounded-xl border">
+                                        <img 
+                                            src={message.senderId === authUser._id ? 
+                                                    authUser.profilePicture || "/vite.svg" 
+                                                    : 
+                                                    selectedUser.profilePicture || "/vite.svg"} 
+                                            alt="Profile picture" />
+                                    </div>
+                                </div>
+                                <div className="chat-header mb-1">
+                                    <time className="text-xs opacity-50 ml-l">
+                                        {formatTime(message.createdAt)}
+                                    </time>
+                                </div>
+                                <div className="chat-bubble flex flex-col">
+                                    {message.image && (
+                                        <img 
+                                            src={message.image} 
+                                            alt="chat" 
+                                            className="sm:max-w-[200px] rounded-md mb-2" 
+                                        />
+                                    )}
+                                    {message.text && <p>{message.text}</p>}
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
 
             <MessageInput />    {/* Display MessageInput */}
