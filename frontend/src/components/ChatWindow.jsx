c3dad63454277da077d336ba33573d3b1d7494ef
Justin Yapjoco <94045038+Shisu123@users.noreply.github.com>
2025-03-19 19:48:01 -0700

    Update ChatWindow.jsx

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -18,7 +18,7 @@ const ChatWindow = () => {
     const {authUser} = useAuthStore();
     const messageEndRef = useRef(null); // Reference to last message for auto scrolling
 
-    // Fetch messages when the selected user changes
+    // Get messages for selected user
     useEffect(() => {
         getMessages(selectedUser._id);
         listenToMessages();
@@ -35,9 +35,8 @@ const ChatWindow = () => {
 
     return (
         <div className="flex-1 flex flex-col overflow-auto">
-            <ChatHeader />
+            <ChatHeader />      {/* Display ChatHeader */}
 
-            {/* Chat messages */}
             <div className="flex-1 overflow-y-auto p-4 space-y-4">
                 {messages.map((message) => (
                     <div
@@ -45,24 +44,42 @@ const ChatWindow = () => {
                         className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
                         ref={messageEndRef} // auto scroll to last message
                     > 
+                        <div className="chat-image avatar">
+                            {/* Display sender's avatar */}
+                            <div className="size-10 rounded-xl border">
+                                <img 
+                                    src={message.senderId === authUser._id ? 
+                                            authUser.profilePicture || "/vite.svg" 
+                                            : 
+                                            selectedUser.profilePicture || "/vite.svg"} 
+                                    alt="Profile picture" />
+                            </div>
+                        </div>
                         <div className="chat-header mb-1">
-                            <span className="font-medium mr-2">
-                                {message.senderId === authUser._id ? authUser.fullName : selectedUser.fullName}
-                            </span>
-                            <time className="text-xs opacity-50">
+                            <time className="text-xs opacity-50 ml-l">
                                 {formatTime(message.createdAt)}
                             </time>
                         </div>
-                        <div className="chat-bubble">
-                            <p>{message.text}</p>
+                        {/* Chat Bubble */}
+                        <div className="chat-bubble flex flex-col">
+                            {/* Display message image */}
+                            {message.image && (
+                                <img 
+                                    src={message.image} 
+                                    alt="chat" 
+                                    className="sm:max-w-[200px] rounded-md mb-2" 
+                                />
+                            )}
+                            {/* Display message text */}
+                            {message.text && <p>{message.text}</p>}
                         </div>
                     </div>
+
                 ))}
             </div>
 
-            <MessageInput />
+            <MessageInput />    {/* Display MessageInput */}
         </div>
     )
 };
-
 export default ChatWindow;
