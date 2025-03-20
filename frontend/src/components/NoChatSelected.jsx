bd31976b724fefe9ecd00d6d5b829fab091c809f
Jordy Ruiz <129150008+Jordy-03@users.noreply.github.com>
2025-03-20 02:13:50 -0700

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -7,7 +7,7 @@ const NoChatSelected = () => {
       <div className="flex-1 flex items-center justify-center">
           <div className="text-center px-4 -space-y-20">
               <img 
-                  src="/public/MalarkyLogo.png" 
+                  src="/MalarkyLogo.png" 
                   alt="Malarky Logo" 
                   className="h-[400px] w-auto object-contain mx-auto"
               />
